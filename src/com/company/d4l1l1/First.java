d9489c71e024bd7d80925e0dfcc46f666a806121
AlexOff <alexov2004@gmail.com>
2021-01-12 23:07:07 +0300

    Решена первая задача 4.1.1;
    http://informatics.mccme.ru/mod/statements/view3.php?chapterid=66

--- src/com/company/d4l1l1/First.java
+++ src/com/company/d4l1l1/First.java
@@ -1,8 +1,23 @@
 package com.company.d4l1l1;
 
+
+import java.util.Scanner;
+
 public class First {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int size = in.nextInt();
+        int[] data = new int[size];
+        int count=0;
 
+        for (int i =0; i<size;i++){
+            data[i] = in.nextInt();
+            if (i>0 && data[i]>data[i-1]){
+                count+=1;
+            }
+        }
+
+        System.out.println(count);
     }
 }
