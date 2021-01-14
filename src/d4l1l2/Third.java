378bda7c679c1c2ffd447a35acfb13151b827def
AlexOff <alexov2004@gmail.com>
2021-01-14 10:15:20 +0300

    Решена третья задача 4.1.2;
    http://informatics.mccme.ru/mod/statements/view3.php?chapterid=742

---
+++ src/d4l1l2/Third.java
@@ -0,0 +1,76 @@
+package d4l1l2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Third {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sizeN = in.nextInt();
+
+
+        ArrayList dataSorted = new ArrayList();
+
+        for (int i = 0; i < sizeN; i++) {
+            dataSorted.add(in.nextInt());
+        }
+
+
+        int sizeK = in.nextInt();
+        ArrayList raw = new ArrayList();
+
+        for (int i = 0; i < sizeK; i++) {
+            raw.add(in.nextInt());
+        }
+        Collections.sort(dataSorted);
+
+        for (int i = 0; i < sizeK; i++) {
+            int[] res = bin(dataSorted, (int) raw.get(i));
+            if (res.length == 2) {
+                System.out.print(res[1]-res[0]+1+" ");
+            } else {
+                System.out.print(0+" ");
+            }
+        }
+
+
+    }
+
+    public static int[] bin(ArrayList list, int pat) {
+        int checkLeft;
+        int checkRight;
+        int mid;
+        int left = 0;
+        int right = list.size() - 1;
+        while (left <= right) {
+            mid = (left + right) / 2;
+
+            if ((int) list.get(mid) == pat) {
+                checkLeft = mid;
+                checkRight = mid;
+
+                while ((checkLeft - 1 >= 0) && ((int) list.get(checkLeft - 1) == pat)) {
+                    checkLeft -= 1;
+                }
+                while ((checkRight + 1 <= list.size() - 1) && ((int) list.get(checkRight + 1) == pat)) {
+                    checkRight += 1;
+                }
+
+                return new int[]{checkLeft + 1, checkRight + 1};
+
+
+            }
+
+            if ((int) list.get(mid) < pat) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+
+        }
+        return new int[]{-1};
+    }
+}
+
+
